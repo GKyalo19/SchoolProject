4398f20d3c71d4f4437510813c5d6034ea42c75f
Grace Kyalo <kyalograce21@gmail.com>
2025-03-26 09:26:14 +0300

    Updated the backend port

--- src/services/api.js
+++ src/services/api.js
@@ -1,43 +1,42 @@
 // api.js
-import axios from 'axios';
-import router from '../router/index';
-import TokenService from './token.service';  //generates tokens
+import axios from 'axios'
+import router from '../router/index'
+import TokenService from './token.service' //generates tokens
 
-const instance = axios.create({     //initializing axios (making requests)
-    baseURL: "http://localhost:8000/api/",  //path to the backend
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
+const instance = axios.create({
+  //initializing axios (making requests)
+  baseURL: 'https://schoolproject-backend.onrender.com/api/', //path to the backend
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
 
 //in postman we had to use a token to make requests
 instance.interceptors.request.use(
-    (config) => {
-      const token = TokenService.getToken(); //first of all we retrieve the token from the local storage
-      if (token) {
-        config.headers["Authorization"] = 'Bearer ' + token;
-      }
-      return config;
-    },
-    (error) => {
-      return Promise.reject(error);
+  (config) => {
+    const token = TokenService.getToken() //first of all we retrieve the token from the local storage
+    if (token) {
+      config.headers['Authorization'] = 'Bearer ' + token
     }
-  );
-
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  },
+)
 
 // Interceptor to handle global authentication errors
 instance.interceptors.response.use(
   (response) => {
-      return response;
+    return response
   },
   (error) => {
-      if (error.response && error.response.status === 401) {
-          TokenService.removeToken();          //if there's an error you're removed from the app and you're redirected to homepage
-          router.push({ name: 'Home' });
-      }
-      return Promise.reject(error);
-  }
-);
+    if (error.response && error.response.status === 401) {
+      TokenService.removeToken() //if there's an error you're removed from the app and you're redirected to homepage
+      router.push({ name: 'Home' })
+    }
+    return Promise.reject(error)
+  },
+)
 
-
-  export default instance;
+export default instance
